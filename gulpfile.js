cf1883858584866e9f1f7e19bf57c7fc37c51cdf
Jesse Reinikka <31450250+JemisaR@users.noreply.github.com>
2020-07-11 21:36:56 +0300

    Changes to building, hiding atlantis-bg

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
     cssimport = require("gulp-cssimport")
-    rename = require('gulp-rename');
+    rename = require('gulp-rename')
+    path  = require('path');
 
 var buildFolder = "./build/";
 
@@ -14,11 +15,21 @@ function buildAsSingleCss(fullPath, newFilename) {
     .pipe(gulp.dest(buildFolder))
 }
 
+var srcPath = "./src"
+    fandomWikiaPath = "fandomwikia"
+    // Note: joining as POSIX as some package does not like '\' in place of '/'
+    fgoPath = path.posix.join(srcPath, fandomWikiaPath, "fate_grand_order")
+
 // Task to build Fate CSS file
 var fate = () => buildAsSingleCss(
-  "./src/fandomwikia/fate_grand_order/main.css",
+  path.posix.join(fgoPath, "main.css"),
   "fate_grand_order"
 )
 
+var fateUserStyles = () => buildAsSingleCss(
+  path.posix.join(fgoPath, "main_userstyles.css"),
+  "fate_grand_order_userstyles"
+)
+
 // Basically => gulp.parallel(example1, example2, example3)
-exports.default = gulp.parallel(fate)
+exports.default = gulp.parallel(fate, fateUserStyles)
